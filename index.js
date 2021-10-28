baa60b63c940a5ce516b479dc70040919c2b97c9
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-28 09:12:33 -0700

    commit tree 00a453bf40c3b4dac4e5c2449724274b4c737ca4

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function fetchBooks() {
+ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(books => renderBooks(books));
 }
+//I am getting the correct answer in my console... but not the terminal
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -10,6 +13,7 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
